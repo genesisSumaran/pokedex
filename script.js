dcac249c44ec28320baa556489941dcde1b8d622
leti <letcalixtollatas@crackthecode.la>
2024-09-20 09:48:31 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+function consumiendoapi() {
+  const numeroPokemon = document
+    .getElementById("holamundo")
+    .value.trim()
+    .toLowerCase();
+
+  // Verificar si el input está vacío
+  if (!numeroPokemon) {
+    Swal.fire("Por favor, introduce un número o nombre de Pokémon.");
+    return;
+  }
+
+  // Comprobar si es un número negativo
+  const numero = parseInt(numeroPokemon);
+  if (numero < 1) {
+    Swal.fire({
+      title: "Es ese un número negativo?",
+      text: "Por favor ingresa un número positivo.",
+      icon: "question",
+    });
+    return;
+  }
+
+  $.ajax({
+    type: "GET",
+    url: `https://pokeapi.co/api/v2/pokemon/${numeroPokemon}`,
+    dataType: "json",
+    success: function (datos) {
+      const { name, id, weight, height, sprites } = datos;
+
+      const html = `
+              <div class="pokemon-card">
+                  <img src="${sprites.front_default}" alt="${name}">
+                  <div class="pokemon-info">
+                      <p>Nombre: ${name}</p>
+                      <p>ID: ${id}</p>
+                      <p>Altura: ${height}</p>
+                      <p>Peso: ${weight}</p>
+                  </div>
+              </div>
+          `;
+
+      $("#contenedor").prepend(html);
+      guardarPokemonEnLocalStorage({ name, id, weight, height, sprites });
+    },
+    error: function () {
+      Swal.fire({
+        title: "No se encontró este Pokémon, inténtalo de nuevo.",
+        width: 600,
+        padding: "3em",
+        color: "#716add",
+        background: "#fff url(/images/trees.png)",
+        backdrop: `
+                  rgba(0,0,123,0.4)
+                  left top
+                  no-repeat
+              `,
+      });
+    },
+  });
+}
